ed1a45491231563bfe4b59c300af0b639419800d
Krishnakumar3k <krishnakumar3kgt@gmail.com>
2023-10-15 12:57:48 +0530

    intialcommit

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,7 +59,7 @@ $(document).ready(function () {
 document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
-            document.title = "Portfolio |Krishna";
+            document.title = "Portfolio|Krishna";
             $("#favicon").attr("href", "assets/images/favicon.png");
         }
         else {
