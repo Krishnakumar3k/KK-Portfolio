8003d22461f7e7914e44237950d145837959f391
Krishnakumar3k <krishnakumar3kgt@gmail.com>
2023-10-15 12:49:42 +0530

    intialcommit

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,12 +59,12 @@ $(document).ready(function () {
 document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
-            document.title = "Portfolio |";
-            $("#favicon").attr("href", "assets/images/");
+            document.title = "Portfolio |Krishna";
+            $("#favicon").attr("href", "assets/images/favicon.png");
         }
         else {
             document.title = "Come Back To Portfolio";
-            $("#favicon").attr("href", "assets/images/favhand.png");
+            $("#favicon").attr("href", "assets/images/favicon.png");
         }
     });
 
